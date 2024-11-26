4058883908c5cd499868ec5e417459eeba5af4ba
Alice <alice@ritsu.dev>
2024-11-26 16:40:43 +1100

    add react router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className='bg-red-200'>Awooo</div>
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 import './index.css'
-import App from './App.tsx'
+import Home from './screens/Home'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
 )
